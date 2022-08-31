74de59cc9bb53af7f7b8722fe1f08c57b817cdb7
M.J <janelidze.marita@kiu.edu.ge>
2022-08-31 22:08:51 +0400

    Initial commit

---
+++ src/Figure/Main.java
@@ -0,0 +1,344 @@
+package Figure;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+public class Main {
+
+	public static void main(String[] args) throws FileNotFoundException, IOException {
+
+		/*
+		 * Input File Paths 
+		 * D:\\TBC_Academy\\Generics\\src\\InputFiles\\Circles_in.dat
+		 * D:\\TBC_Academy\\Generics\\src\\InputFiles\\Rectangles_in.dat
+		 * D:\\TBC_Academy\\Generics\\src\\InputFiles\\Triangles_in.dat
+		 * 
+		 * 15.07.2022 homework
+		 * 
+		 */
+		// Task 1
+		ArrayList<Circle> circleArrayList = new ArrayList<>(CreateArrayList.circlesArrayList(args[0]));
+		
+		long result1 = circleArrayList.stream().filter(el -> el.getRadius() >= 10).count();
+		
+		//Task 2
+		ArrayList<Rectangle> rectangleArrayList = new ArrayList<>(CreateArrayList.rectanglesArrayList(args[1]));
+		
+		ArrayList<Double> result2 = new ArrayList<>();
+		
+		rectangleArrayList.stream().filter(p -> p.getArea() >= 100).forEach(p -> result2.add(p.getLength()));
+		
+		//Task 3
+		ArrayList<Triangle> triangleleArrayList = new ArrayList<>(CreateArrayList.trianglesArrayList(args[2]));
+		
+		triangleleArrayList.stream().filter(p -> p.isRightTriangle()).forEach(el -> System.out.println(el.getPerimeter()));
+		
+
+		Set<Circle> circleSet = CreateTreesets.circlesTreeSet(args[0]);
+		/*
+		 * ok 4. Circle-ების Set-ში იპოვეთ მაქსიმალური ფართობის 
+და მინიმალური წრეწირის სიგრძის მნიშვნელობები.
+		 */
+		System.out.println("Maximum Area: " + circleSet.stream().sorted().findFirst().get().getArea());
+		
+		
+		
+//		Set<Rectangle> rectangleSet = CreateTreesets.rectanglesTreeSet(args[1]);
+//		Set<Triangle> triangleleSet = CreateTreesets.trianglesTreeSet(args[2]);
+//
+//		getCirlce(circleSet);
+
+
+
+
+
+	}
+	
+	public static void getCirlce(Set<Circle> circle) {
+		TreeSet<Circle> helper = (TreeSet) circle;
+		Circle max = helper.first();
+		Circle min = helper.first();
+		for (Circle element : helper) {
+			if (element.getArea() > max.getArea()) {
+				max = element;
+			}
+			if(element.getPerimeter() < min.getPerimeter()) {
+				min = element;
+			}
+		}
+		System.out.println("Maximum Circle Area is: " + max.getArea());
+		System.out.println("Minimum Circle Perimter is: " + min.getPerimeter());
+	}
+
+	
+
+	
+	public static ArrayList<Figure> getMinMax(ArrayList<Circle> circleArrayList,
+			ArrayList<Rectangle> rectangleArrayList, ArrayList<Triangle> triangleleArrayList) {
+
+		ArrayList<Figure> result = new ArrayList<>();
+
+		try {
+
+			Collections.sort(circleArrayList);
+			Collections.sort(rectangleArrayList);
+			Collections.sort(triangleleArrayList);
+
+			result.add(circleArrayList.get(0));
+			result.add(circleArrayList.get(circleArrayList.size() - 1));
+
+			result.add(rectangleArrayList.get(0));
+			result.add(rectangleArrayList.get(rectangleArrayList.size() - 1));
+
+			result.add(triangleleArrayList.get(triangleleArrayList.size() - 1));
+			result.add(triangleleArrayList.get(0));
+
+		} catch (Exception e) {
+			System.err.println("Some Figure is missing!");
+		}
+
+		return result;
+
+	}
+
+	public static TreeSet<Figure> getMinMax(TreeSet<Figure> figureTreeset) {
+		TreeSet<Figure> result = new TreeSet<>();
+		try {
+			TreeSet<Circle> circleTreeset = new TreeSet<>();
+			TreeSet<Rectangle> rectangleTreeset = new TreeSet<>();
+			TreeSet<Triangle> triangleleTreeset = new TreeSet<>();
+			for (Figure element : figureTreeset) {
+				if (element instanceof Circle) {
+					circleTreeset.add((Circle) element);
+				} else if (element instanceof Triangle) {
+					triangleleTreeset.add((Triangle) element);
+				} else
+					rectangleTreeset.add((Rectangle) element);
+			}
+
+			result.add(circleTreeset.last());
+			result.add(circleTreeset.first());
+			result.add(rectangleTreeset.last());
+			result.add(rectangleTreeset.first());
+			result.add(triangleleTreeset.first());
+			result.add(triangleleTreeset.last());
+			// prints the max rectangle, triangle and circle as requested in ex 2.
+			System.out.println("Max Circle: Circle {radius=" + circleTreeset.last().getRadius() + "}"
+					+ " Max Rectangle: " + rectangleTreeset.last() + " Max Triangle: " + triangleleTreeset.first());
+			// returns a treeset of figures with min and max triangle, circle and rectangle
+
+		} catch (Exception e) {
+			System.err.println("Some Figure is missing!");
+		}
+		return result;
+	}
+
+	public static Figure getMinMax(TreeSet<Circle> circleTreeset, TreeSet<Rectangle> rectangleTreeset,
+			TreeSet<Triangle> triangleleTreeset) {
+
+		TreeSet<Figure> result = new TreeSet<>();
+
+		try {
+			result.add(circleTreeset.last());
+			result.add(circleTreeset.first());
+			result.add(rectangleTreeset.last());
+			result.add(rectangleTreeset.first());
+			result.add(triangleleTreeset.first());
+			result.add(triangleleTreeset.last());
+		} catch (Exception e) {
+			System.err.println("Some Figure is missing!");
+		}
+		return result.last();
+	}
+
+}
+
+class CreateTreesets {
+
+	public static TreeSet<Circle> circlesTreeSet(String args) throws FileNotFoundException, IOException {
+		Pattern formatCircle = Pattern.compile("([0-9]+)");
+
+		try (FileInputStream inStrm = new FileInputStream(args);
+				BufferedInputStream bf = new BufferedInputStream(inStrm);
+				BufferedReader read = new BufferedReader(new InputStreamReader(bf, "UTF-8"));
+				FileOutputStream outStrm = new FileOutputStream(
+						"D:\\TBC_Academy\\Generics\\src\\OutputFiles\\Circles_out_unique.dat");
+				OutputStreamWriter bfo = new OutputStreamWriter(outStrm);
+				BufferedWriter bfw = new BufferedWriter(bfo);) {
+			TreeSet<Circle> setCircle = new TreeSet<Circle>();
+			while (read.ready()) {
+				String str = read.readLine();
+				Matcher match = formatCircle.matcher(str);
+				if (match.matches()) {
+					setCircle.add(new Circle("Circle", Double.parseDouble(str)));
+				}
+			}
+			/*
+			 * Iterator<Circle> circles = setCircle.iterator(); while (circles.hasNext()) {
+			 * bfw.write(Double.toString(circles.next().getRadius()) + "\n");
+			 * 
+			 * }
+			 */
+			return setCircle;
+		}
+	}
+
+	public static TreeSet<Rectangle> rectanglesTreeSet(String args) throws FileNotFoundException, IOException {
+		Pattern formatRectangle = Pattern.compile("([0-9]+[-][0-9]+)");
+		try (FileInputStream inStrm = new FileInputStream(args);
+				BufferedInputStream bf = new BufferedInputStream(inStrm);
+				BufferedReader read = new BufferedReader(new InputStreamReader(bf, "UTF-8"));
+				FileOutputStream outStrm = new FileOutputStream(
+						"D:\\TBC_Academy\\Generics\\src\\OutputFiles\\Rectangles_out.dat");
+				OutputStreamWriter bfo = new OutputStreamWriter(outStrm);
+				BufferedWriter bfw = new BufferedWriter(bfo);) {
+			TreeSet<Rectangle> setRectangle = new TreeSet<Rectangle>();
+			while (read.ready()) {
+				String str = read.readLine();
+				Matcher match = formatRectangle.matcher(str);
+				if (match.matches()) {
+					String[] lst = str.split("-");
+					setRectangle.add(new Rectangle(Double.parseDouble(lst[0]), Double.parseDouble(lst[1])));
+
+				}
+			}
+			/*
+			 * for(Rectangle rec : setRectangle){ bfw.write(rec.getLength() + "-" +
+			 * rec.getHigth() + "\n"); System.out.println(rec.getPerimeter()); }
+			 */
+			return setRectangle;
+		}
+
+	}
+
+	public static TreeSet<Triangle> trianglesTreeSet(String args) throws FileNotFoundException, IOException {
+		Pattern formatTriangle = Pattern.compile("([0-9]+[-][0-9]+[-][0-9]+)");
+		try (FileInputStream inStrm = new FileInputStream(args);
+				BufferedInputStream bf = new BufferedInputStream(inStrm);
+				BufferedReader read = new BufferedReader(new InputStreamReader(bf, "UTF-8"));
+				FileOutputStream outStrm = new FileOutputStream(
+						"D:\\TBC_Academy\\Generics\\src\\OutputFiles\\Triangles_out.dat.dat");
+				OutputStreamWriter bfo = new OutputStreamWriter(outStrm);
+				BufferedWriter bfw = new BufferedWriter(bfo);) {
+			TreeSet<Triangle> setTriangle = new TreeSet<Triangle>();
+
+			while (read.ready()) {
+				String str = read.readLine();
+				Matcher match = formatTriangle.matcher(str);
+				if (match.matches()) {
+					String[] lst = str.split("-");
+					setTriangle.add(new Triangle(Double.parseDouble(lst[0]), Double.parseDouble(lst[1]),
+							Double.parseDouble(lst[2])));
+				}
+			}
+			/*
+			 * for(Triangle triangle : setTriangle){ bfw.write(triangle.getAB() + "-" +
+			 * triangle.getBC() + "-" + triangle.getCA() + "\n"); }
+			 */
+			return setTriangle;
+		}
+	}
+
+}
+
+class CreateArrayList {
+
+	public static ArrayList<Circle> circlesArrayList(String args) throws FileNotFoundException, IOException {
+		Pattern formatCircle = Pattern.compile("([0-9]+)");
+
+		try (FileInputStream inStrm = new FileInputStream(args);
+				BufferedInputStream bf = new BufferedInputStream(inStrm);
+				BufferedReader read = new BufferedReader(new InputStreamReader(bf, "UTF-8"));
+				FileOutputStream outStrm = new FileOutputStream(
+						"D:\\TBC_Academy\\Generics\\src\\OutputFiles\\Circles_out_unique.dat");
+				OutputStreamWriter bfo = new OutputStreamWriter(outStrm);
+				BufferedWriter bfw = new BufferedWriter(bfo);) {
+			ArrayList<Circle> setCircle = new ArrayList<>();
+			while (read.ready()) {
+				String str = read.readLine();
+				Matcher match = formatCircle.matcher(str);
+				if (match.matches()) {
+					setCircle.add(new Circle("Circle", Double.parseDouble(str)));
+				}
+			}
+			/*
+			 * Iterator<Circle> circles = setCircle.iterator(); while (circles.hasNext()) {
+			 * bfw.write(Double.toString(circles.next().getRadius()) + "\n");
+			 * 
+			 * }
+			 */
+			return setCircle;
+		}
+	}
+
+	public static ArrayList<Rectangle> rectanglesArrayList(String args) throws FileNotFoundException, IOException {
+		Pattern formatRectangle = Pattern.compile("\\d{1,}-\\d{1,}");
+		try (FileInputStream inStrm = new FileInputStream(args);
+				BufferedInputStream bf = new BufferedInputStream(inStrm);
+				BufferedReader read = new BufferedReader(new InputStreamReader(bf, "UTF-8"));
+				FileOutputStream outStrm = new FileOutputStream(
+						"D:\\TBC_Academy\\Generics\\src\\OutputFiles\\Rectangles_out.dat");
+				OutputStreamWriter bfo = new OutputStreamWriter(outStrm);
+				BufferedWriter bfw = new BufferedWriter(bfo);) {
+			ArrayList<Rectangle> setRectangle = new ArrayList<Rectangle>();
+			while (read.ready()) {
+				String str = read.readLine();
+				Matcher match = formatRectangle.matcher(str);
+				if (match.matches()) {
+					String[] lst = str.split("-");
+					setRectangle.add(new Rectangle(Double.parseDouble(lst[0]), Double.parseDouble(lst[1])));
+
+				}
+			}
+			/*
+			 * for(Rectangle rec : setRectangle){ bfw.write(rec.getLength() + "-" +
+			 * rec.getHigth() + "\n"); System.out.println(rec.getPerimeter()); }
+			 */
+			return setRectangle;
+		}
+
+	}
+
+	public static ArrayList<Triangle> trianglesArrayList(String args) throws FileNotFoundException, IOException {
+		Pattern formatTriangle = Pattern.compile("([0-9]+[-][0-9]+[-][0-9]+)");
+		try (FileInputStream inStrm = new FileInputStream(args);
+				BufferedInputStream bf = new BufferedInputStream(inStrm);
+				BufferedReader read = new BufferedReader(new InputStreamReader(bf, "UTF-8"));
+				FileOutputStream outStrm = new FileOutputStream(
+						"D:\\TBC_Academy\\Generics\\src\\OutputFiles\\Triangles_out.dat.dat");
+				OutputStreamWriter bfo = new OutputStreamWriter(outStrm);
+				BufferedWriter bfw = new BufferedWriter(bfo);) {
+			ArrayList<Triangle> setTriangle = new ArrayList<Triangle>();
+
+			while (read.ready()) {
+				String str = read.readLine();
+				Matcher match = formatTriangle.matcher(str);
+				if (match.matches()) {
+					String[] lst = str.split("-");
+					setTriangle.add(new Triangle(Double.parseDouble(lst[0]), Double.parseDouble(lst[1]),
+							Double.parseDouble(lst[2])));
+				}
+			}
+			/*
+			 * for(Triangle triangle : setTriangle){ bfw.write(triangle.getAB() + "-" +
+			 * triangle.getBC() + "-" + triangle.getCA() + "\n"); }
+			 */
+			return setTriangle;
+		}
+	}
+
+}
---
+++ src/Figure/Triangle.java
@@ -0,0 +1,138 @@
+package Figure;
+
+public class Triangle extends Figure{
+
+	private double ab;
+	private double bc;
+	private double ca;
+	private double height; //height of triangle
+	private double s;
+	private double angle1;
+	private double angle2;
+	
+	public Triangle() {
+		this(3,4,5);
+	}
+
+	public Triangle(double ab, double bc, double ca) {
+		super("Hello there! Triangle is created!");
+		this.ab = ab;
+		this.bc = bc;
+		this.ca = ca;
+		this.s = (ab + bc + ca)/2;
+		this.height = 2 * Math.sqrt(s * (s - ab) * (s - bc) * (s - ca)) / ca ;  //formula to calculate height
+		this.angle1 = Math.acos((bc * bc + ca * ca - ab * ab)/(2 * bc * ca));
+		this.angle2 = Math.acos((bc * bc + ab * ab - ca * ca)/(2 * bc * ab));
+	}
+
+	public double getAB() {
+		return ab;
+	}
+
+	public void setAB(double ab) {
+		this.ab = ab;
+	}
+
+	public double getBC() {
+		return bc;
+	}
+
+	public void setBC(double bc) {
+		this.bc = bc;
+	}
+
+	public double getCA() {
+		return ca;
+	}
+
+	public void setCA(double ca) {
+		this.ca = ca;
+	}
+	@Override
+	public double getPerimeter() {
+		return ab + bc + ca;
+	}
+	@Override
+	public double getArea() {
+		return (this.height * ca) / 2;
+	}
+	
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Triangle {ab=");
+		builder.append(ab);
+		builder.append(", bc=");
+		builder.append(bc);
+		builder.append(", ca=");
+		builder.append(ca);
+		builder.append("}");
+		return builder.toString();
+	}
+
+	public boolean equals(Triangle obj1, Triangle obj2) {
+		if (!(obj1 instanceof Figure) || !(obj2 instanceof Figure) || obj1 == null || obj2 == null)
+			return false;
+		return obj1.getPerimeter() == obj2.getPerimeter();
+	}
+	
+	public int getTriangleType() {
+		if(this.ab >= this.bc && this.ab >= this.ca) {
+			if(Math.sqrt(this.bc * this.bc + this.ca * this.ca) == ab){
+				return 0;
+			}else if(Math.sqrt(this.bc * this.bc + this.ca * this.ca) < ab) {
+				return -1;
+			}else return 1;
+		}else if(this.bc >= this.ab && this.bc >= this.ca) {
+			if(Math.sqrt(this.ab * this.ab + this.ca * this.ca) == this.bc){
+				return 0;
+			}else if(Math.sqrt(this.ab * this.ab + this.ca * this.ca) < this.bc) {
+				return -1;
+			}else return 1;
+		}else {
+			if(Math.sqrt(this.ab * this.ab + this.bc * this.bc) == this.ca){
+				return 0;
+			}else if(Math.sqrt(this.ab * this.ab + this.bc * this.bc) < this.ca) {
+				return -1;
+			}else return 1;
+		}
+	}
+
+	@Override
+	public void printFigureData() {
+		System.out.println("Side 1: " + ab + " Side 2: " + bc + " Side 3: " + ca);
+	}
+
+	@Override
+	public void sayHelloToFigure() {
+		System.out.println("Hello from Triangle");
+		
+	}
+
+	@Override
+	public boolean validateFigure() {
+		if(ab + bc > ca && ab + ca > bc && bc + ca > ab && ab > 0 && bc > 0 && ca >0) {
+			return true;
+		}
+		return false;
+	}
+	/*
+	 Task 1. 06.07.2022
+	 
+	 Triangle does not have a getLength method but has a getPerimeter so I used that.
+	 */
+
+	@Override
+	public int compareTo(Figure o) {
+
+		return Double.compare(o.getPerimeter(), this.getPerimeter());
+	}
+	public boolean isRightTriangle() {
+		if((ab * ab) == bc * bc + ca * ca || (bc * bc) == ab * ab + ca * ca  
+				|| (ca * ca) == ab * ab + bc * bc) {
+			return true;
+		}
+		return false;
+	}
+}
